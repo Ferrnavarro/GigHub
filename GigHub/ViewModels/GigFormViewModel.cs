692d136079897d5f7ca2ce8c038ba2d4217ea101
maganf01 <fernando.magana@tecoloco.com>
2020-10-14 15:11:37 -0600

    Create Add a Gig form

---
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GigHub.Models;
+using Microsoft.Owin.Security.Notifications;
+
+namespace GigHub.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
